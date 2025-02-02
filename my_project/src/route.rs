4325601f6f777889c0b3d724bf57ce8259ce816c
Massinissa Amalou <massi@Mac-mini-de-Massinissa.local>
2025-02-02 15:40:04 +0100

    remove generated project

--- my_project/src/route.rs
+++
@@ -1,7 +0,0 @@
-use axum::Router;
-
-pub fn configure() -> Router {
-    Router::new()
-        .route("/", get(|| async { "Nebulis Backend API" }))
-        .route("/hello", get(|| async { "Hello, World!" }));
-}
